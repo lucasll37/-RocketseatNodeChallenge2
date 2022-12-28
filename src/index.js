d4aea719f37657373fa393b502603a042e503c3f
root <root@DESKTOP-J4HP0MI>
2022-12-27 21:08:27 -0300

    finished project

--- src/index.js
+++ src/index.js
@@ -10,19 +10,57 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+  if(!user) {
+    return response.status(404).json({ error: "usernameAlreadyExists" })
+  }
+  request.user = user
+  return next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+  if(!user.pro && user.todos.length >= 10) {
+    return response.status(403).json({error: "user free and todos have above 10 items"})
+  }
+
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+  const user = users.find(user => user.username === username)
+
+  if(!validate(id)) {
+    return response.status(400).json({error: "The id isn't uuid!"})
+  }
+
+  if(!user){
+    return response.status(404).json({error: "Bad Request"})
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+  if(!todo) {
+    return response.status(404).json({error: "Bad Request"})
+  }
+
+  request.todo = todo
+  request.user = user
+
+  return next()
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+  const user = users.find(user => user.id === id)
+  if(!user) {
+    return response.status(404).json({ error: "usernameAlreadyExists" })
+  }
+  request.user = user
+  return next()
 }
 
 app.post('/users', (request, response) => {
